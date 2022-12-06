abbb3ac6f75bb83d980cb69431237225eff4d57a
ridwanmahendra <ridwanmahenra@teknokrat.ac.id>
2022-12-06 19:03:32 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# jdjdjjdd
+import tkinter as tk
+import customtkinter as ck
+
+import pandas as np
+import numpy as np
+import pickle
+
+import mediapipe as mp
+import cv2
+from PIL import Image, ImageTk
+
